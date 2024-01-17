fc9951797a85bcead1e36f8fcfb32fa2c3be5119
sunkezheng <sunkezheng@outlook.com>
2024-01-17 11:27:23 +0800

    2024-01-17 11:27:22

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,6 @@
 
 hugo
 
-cd ./public
-
-rm -rf .git
-
-git init
-git remote add origin git@github.com:sunkz/blog.git
 git add .
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push -f --set-upstream origin main
-
-rm -rf *
-rm -rf .git
-
-cd ..
-
-git add .
-git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
-git push
